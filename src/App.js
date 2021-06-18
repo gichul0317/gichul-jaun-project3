e829a4b3b4e0d5987d50d916a69912378e0b5cd2
gichul <gichul0317@gmail.com>
2021-06-17 23:23:42 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import virus from './assets/virus.svg';
+import player from './assets/player.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <h1>Vid-Monster</h1>
+        <h2>set up in progress...</h2>
+        <img src={virus} alt="" />
+        <img src={player} alt="" />
+      </div>
     </div>
   );
 }
